73c8d4254e38c8afd66a0a318dd7753fbb5cfb5d
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2022-04-22 15:02:36 +0300

    fetched the api

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-const input =  document.querySelector("input")
+const button = document.querySelector("button")
+button.addEventListener("click", getFetch)
+
+function getFetch(){
+    const spellRequest = document.querySelector("input").value
+
+    const url = `https://www.dnd5eapi.co/api/spells/${spellRequest}`
+
+    fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
